d8c34f7956f461bcd4ca49ce227158ceee6bfdc7
semako123 <zosusemenoch@gmail.com>
2023-01-28 23:01:07 +0100

    Add mobile responsiveness to navbar Install react icons

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,9 +2,12 @@ import React from "react";
 import "./navbar.css";
 import { AnimationOnScroll } from "react-animation-on-scroll";
 import { logo } from "../../assets/images";
+import { RiMenu3Line, RiCloseLine } from "react-icons/ri";
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 const Navbar = () => {
+  const [toggle, setToggle] = useState(false);
   const navigate = useNavigate();
   return (
     <AnimationOnScroll
@@ -36,10 +39,57 @@ const Navbar = () => {
           >
             <a>Programs</a>
           </p>
-          <p onClick={() => {navigate("/partnership")}}>
+          <p
+            onClick={() => {
+              navigate("/partnership");
+            }}
+          >
             <a>Partnership</a>
           </p>
         </div>
+        <div>
+          {toggle ? (
+            <RiCloseLine
+              color="white"
+              size={27}
+              onClick={() => {
+                setToggle(false);
+              }}
+            />
+          ) : (
+            <RiMenu3Line
+              color="white"
+              size={27}
+              onClick={() => {
+                setToggle(true);
+              }}
+            />
+          )}
+        </div>
+        {toggle && (
+          <div className="ghit__navbar-links_mobile scale-up-center">
+            <p>
+              <a href="/#about">About Us</a>
+            </p>
+            <p>
+              <a href="/#wwo">WWO</a>
+            </p>
+            <p
+              onClick={() => {
+                navigate("/programs");
+              }}
+            >
+              <a>Programs</a>
+            </p>
+            <p
+              onClick={() => {
+                navigate("/partnership");
+              }}
+            >
+              <a>Partnership</a>
+            </p>
+          </div>
+        )}
       </div>
     </AnimationOnScroll>
   );
